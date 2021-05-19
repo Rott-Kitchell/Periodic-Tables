d4283d0342db865513cd4a9f413e0851295c4392
RottK <r.kitchell815@gmail.com>
2021-05-19 12:41:04 -0500

    US-02 tests pass

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -22,6 +22,36 @@ function hasValidFields(req, res, next) {
       message: `Invalid field(s): ${invalidFields.join(", ")}`,
     });
   }
+  const reserveDate = new Date(
+      `${data.reservation_date} ${data.reservation_time} GMT-0500`
+    ),
+    start = new Date(`${data.reservation_date} 10:30:00 GMT-0500`),
+    end = new Date(`${data.reservation_date} 21:30:00 GMT-0500`);
+
+  const todaysDate = new Date();
+
+  if (reserveDate.getDay() === 2) {
+    return next({
+      status: 400,
+      message:
+        "Reservations cannot be made on a Tuesday (Restaurant is closed).",
+    });
+  }
+  if (reserveDate < todaysDate) {
+    return next({
+      status: 400,
+      message: "Reservations must be made in the future.",
+    });
+  }
+  if (
+    reserveDate.getTime() < start.getTime() ||
+    reserveDate.getTime() > end.getTime()
+  ) {
+    return next({
+      status: 400,
+      message: "Reservations cannot be made outside of 10:30am to 9:30pm.",
+    });
+  }
 
   next();
 }
