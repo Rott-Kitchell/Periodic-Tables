509dd51dc797575141e59c01135266a4239e811b
RottK <r.kitchell815@gmail.com>
2021-05-25 09:31:07 -0500

    almost done with US-04

--- front-end/src/reservations/Seat.js
+++ front-end/src/reservations/Seat.js
@@ -1,20 +1,47 @@
-import { useParams } from "react-router";
+import { useState } from "react";
+import { useHistory, useParams } from "react-router";
+import ErrorAlert from "../layout/ErrorAlert";
+import { seatResAtTable } from "../utils/api";
 
 export default function Seat({ tables, reservations }) {
+  const initialState = { table_id: 0 };
+  const [formData, setFormData] = useState(initialState);
+  const [error, setError] = useState(null);
   const { reservation_id } = useParams();
+  const history = useHistory();
 
-  const singleRes = reservations.find(
-    (res) => res.reservation_id === reservation_id
-  );
+  const singleRes = reservations.find((res) => {
+    return res.reservation_id === Number(reservation_id);
+  });
+
+  let handleSubmit = (event) => {
+    event.preventDefault();
+    if (formData.table_id > 0) {
+      seatResAtTable(formData.table_id, reservation_id)
+        .then(setFormData(initialState))
+        .then(history.push(`/dashboard`))
+        .catch(setError);
+    } else {
+      setError({ message: "Not a valid table" });
+    }
+  };
+
+  const handleChange = ({ target }) => {
+    setFormData({
+      ...formData,
+      [target.name]: target.value,
+    });
+  };
+
   const tableMenu = tables ? (
-    tables.forEach((table) => {
-      if (!table.reservation_id && singleRes.people <= table.capacity) {
+    tables.map((table) => {
+      if (table.reservation_id === null && singleRes.people <= table.capacity) {
         return (
-          <option value={table.table_id}>
+          <option value={table.table_id} key={table.table_id}>
             {table.table_name} - {table.capacity}
           </option>
         );
-      }
+      } else return "";
     })
   ) : (
     <option defaultValue>No available tables</option>
@@ -22,9 +49,23 @@ export default function Seat({ tables, reservations }) {
 
   return (
     <div>
-      <select className="form-select" name="table_id">
-        {tableMenu}
-      </select>
+      <h2>Seat Reservation</h2>
+      <ErrorAlert error={error} />
+      <form onSubmit={handleSubmit}>
+        <label className="form-label" htmlFor="table_id">
+          Select Table:&nbsp;
+        </label>
+        <select className="form-select" name="table_id" onChange={handleChange}>
+          <option defaultValue>Please choose table:</option>
+          {tableMenu}
+        </select>
+        <button className="btn btn-primary" type="submit">
+          Submit
+        </button>
+        <button className="btn btn-secondary" onClick={history.goBack}>
+          Cancel
+        </button>
+      </form>
     </div>
   );
 }
